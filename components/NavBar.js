236a1c5c7b772f0e692f7d25f7fa8897a7c88d83
Shugi Yen Lazala <shugi.lazala@yahoo.com>
2021-03-03 22:30:45 +0900

    initial

---
+++ components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div
+      className='relative bg-center bg-cover h-screen min-h-144'
+      style={{
+        backgroundImage: `url(
+          'https://images.unsplash.com/photo-1536300007881-7e482242baa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=80'
+        )`,
+      }}
+    >
+      heyh
+    </div>
+  );
+};
+
+export default NavBar;
